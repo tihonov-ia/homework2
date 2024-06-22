9336e163c8ab92f34a36da16b1fdd664be820503
tihonov-ia <tia22@yandex.ru>
2024-06-23 02:53:26 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,7 +1,5 @@
-first_name = 'Курс:Pyton,'
-homework_count =12
-hours_count = 1.5
-a = 12
-b = 1.5
-c=b/a
-print(first_name,'всего задач:',homework_count,',затрачено часов:',hours_count,', среднее время выполнения:',c)
+str = 'Python,'
+task = 12
+hours = 1.5
+print('Курс:',str,'всего задач:',task,', затрачено часов:',
+      hours,', среднее время выполнения:',hours/task)
